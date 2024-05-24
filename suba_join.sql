e3487fa3f41a37c301b9d9a4c051a45ff542b752
Ben Nestor <7893322+bennestor@users.noreply.github.com>
2024-05-24 14:48:05 +0800

    Update suba_join.sql

--- suba_join.sql
+++ suba_join.sql
@@ -1,7 +1,11 @@
 --Add information from SUBA predictions for homologous Arabidopsis protein locations (including updated Arabidopsis hit annotation)
 CREATE TABLE 'genome_spreadsheet_sql_suba' AS 
-SELECT Gene_id, SwissProt_annot, a.field2 AS 'Arabidopsis_hit_annot', suba_location_consensus AS 'Arabidopsis_location_suba', 
+SELECT Gene_id, SwissProt_annot, a.field2 AS 'Arabidopsis_hit_annot', suba_location_consensus AS 'Arabidopsis_location_suba',
 suba_location_all_predictors AS 'Arabidopsis_location_suba_all_predictors', Sequence_length, Sequence 
 FROM genome_spreadsheet_sql g LEFT JOIN at_blast_filt a ON g.Gene_id=a.field1
 LEFT JOIN tair_blast_filt_ids t ON g.Gene_id=t.field1
 LEFT JOIN Suba_all s ON t.field2=s.locus;
+
+--Make second table for all of the SUBA predictions
+CREATE TABLE 'spreadsheet_suba_all_predictions' AS
+SELECT * FROM tair_blast_filt_ids t LEFT JOIN Suba_all_allpredictions s ON t.field2=s.locus;
